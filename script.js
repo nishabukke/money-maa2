e1508720d4a825a3136ed9061c3dd5be04e8b349
Nisha Bukke <sononenisha827@gmail.com>
2023-12-22 12:51:38 +0530

    added inner pages

--- script.js
+++ script.js
@@ -139,3 +139,28 @@ $('.menu ul .nav__submenu').click(function(e) {
   
   $(this).siblings().addClass('is-active');
 });
+
+
+
+function activeTab(evt, id) {
+           
+  // Get all elements with class="tablinks" and remove the class "active"
+   let tabactive = document.getElementsByClassName("TabButtonSelected");
+   tabactive[0].className = tabactive[0].className.replace(" TabButtonSelected", "");
+
+   document.getElementById(id).style.display = "block";
+   evt.currentTarget.className += " TabButtonSelected";
+
+   displaySection(evt,id)
+}
+
+function displaySection(evt, id) {
+
+   let tabactive = document.getElementsByClassName("tab-section");
+   tabactive[0].className = tabactive[0].className.replace(" d-chart-show", "d-chart-n");
+   // add below line of codes
+   [...document.querySelectorAll('div.tab-section')].forEach(item => item.style.display='none')
+   document.getElementById("Section" + id).style.display = "block";
+   evt.currentTarget.className += " d-chart-show";
+
+}
