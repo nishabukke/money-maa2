68a0e53d9cbbd390adf31149a2ad04929ac1d4ce
Nisha Bukke <sononenisha827@gmail.com>
2024-02-28 16:10:16 +0530

    money maa changes

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-      items: 1,
-      margin: 10,
-      loop: true
-    });
+
+// mobile menu
+
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".hamburger" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".hamburger" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".hamburger" ).show();
+  });
+  });
+  
   });
 
+ // Close out sub menu
+ $('.sub__close').click(function(e) {
+  e.preventDefault();
+  
+  $(this).parent().parent().removeClass('is-active');
+});
 
-  var counted = 0;
+// Trigger sub menu
+$('.menu ul .nav__submenu').click(function(e) {
+  e.preventDefault();
+  
+  $(this).siblings().addClass('is-active');
+});
+
+var counted = 0;
 $(window).scroll(function() {
 
   var count = $('#counter-about');
@@ -51,6 +77,8 @@ $(window).on("scroll touchmove", function() {
 
   if ($(document).scrollTop() > $("#sticky-wrapper").position().top) {
     $('.sticky-area').css('width', '100%');
+    $('.sticky-area').css('max-width', '1600px');
+    $('.sticky-area').css('margin', '0 auto');
     $('.sticky-area').css('position', 'fixed');
     $('.sticky-area').css('top', '0px');
     $('.sticky-area').css('background', '#ecf5f7');
@@ -104,41 +132,6 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 
 
-// mobile menu
-
-$( document ).ready(function() {
-
-  $( ".cross" ).hide();
-  $( ".menu" ).hide();
-  $( ".hamburger" ).click(function() {
-  $( ".menu" ).slideToggle( "slow", function() {
-  $( ".hamburger" ).hide();
-  $( ".cross" ).show();
-  });
-  });
-  
-  $( ".cross" ).click(function() {
-  $( ".menu" ).slideToggle( "slow", function() {
-  $( ".cross" ).hide();
-  $( ".hamburger" ).show();
-  });
-  });
-  
-  });
-
- // Close out sub menu
- $('.sub__close').click(function(e) {
-  e.preventDefault();
-  
-  $(this).parent().parent().removeClass('is-active');
-});
-
-// Trigger sub menu
-$('.menu ul .nav__submenu').click(function(e) {
-  e.preventDefault();
-  
-  $(this).siblings().addClass('is-active');
-});
 
 
 
